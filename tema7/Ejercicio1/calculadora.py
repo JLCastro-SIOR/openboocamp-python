48ae8f2e9925d738abf4763f2b7f161b211398ad
José Luis Luis <112734405+JLCastro-SIOR@users.noreply.github.com>
2023-01-12 20:02:20 +0000

    Agregando ejercicio 1 del tema 7 al repositorio bootcamp

---
+++ tema7/Ejercicio1/calculadora.py
@@ -0,0 +1,19 @@
+def sum(a,b):
+  result = a+b
+  return result
+
+def res(a,b):
+  result = a-b
+  return result
+
+def mult(a,b):
+  result = a*b
+  return result
+
+def div(a,b):
+  result = 0
+  if b==0:
+    result = 0
+  else:
+    result = a/b
+  return result
---
+++ tema7/Ejercicio1/main.py
@@ -0,0 +1,9 @@
+import calculadora as calc
+
+a = 6
+b = 4
+
+print(calc.sum(a,b))
+print(calc.res(a,b))
+print(calc.div(a,b))
+print(calc.mult(a,b))
